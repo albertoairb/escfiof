708601cb024f1bf34a9b4f252e4e522d8237c72e
Alberto Franzini Neto <albertoairb@gmail.com>
2026-08-26 14:57:51 -0300

    Corrige nome de exibicao de Antonio Ovidio Ferrucio Cardoso

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ const OFFICERS = [
   { canonical_name: "Daniel Alves de Siqueira", rank: "1º Ten PM", name: "Daniel Alves de Siqueira", aliases: ["Siqueira"] },
   { canonical_name: "Fernanda Bruno Pomponio Martignago", rank: "1º Ten Dent PM", name: "Fernanda Bruno Pomponio Martignago", aliases: ["Pomponio"] },
   { canonical_name: "Dayana de Oliveira Silva Almeida", rank: "1º Ten Dent PM", name: "Dayana de Oliveira Silva Almeida", aliases: ["Dayana"] },
-  { canonical_name: "Antonio Ovidio Ferruccio Cardoso", rank: "1º Ten PM", name: "Antonio Ovidio Ferruccio Cardoso", aliases: ["Ferrucio"] },
+  { canonical_name: "Antonio Ovidio Ferruccio Cardoso", rank: "1º Ten PM", name: "Antonio Ovidio Ferrucio Cardoso", aliases: ["Ferrucio", "Antonio Ovidio Ferrucio Cardoso"] },
   { canonical_name: "Bruno Antao de Oliveira", rank: "1º Ten PM", name: "Bruno Antao de Oliveira", aliases: ["Antao"] },
   { canonical_name: "Larissa Amadeu Leite", rank: "1º Ten PM", name: "Larissa Amadeu Leite", aliases: ["Amadeu"] },
   { canonical_name: "Renato Fernandes Freire", rank: "1º Ten PM", name: "Renato Fernandes Freire", aliases: ["Freire"] },
